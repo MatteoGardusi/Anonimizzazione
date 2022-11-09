331143189608d379b949b470f0305f9f115df281
Matteo Gardusi <matteo.gardusigmail.com>
2022-11-09 17:50:53 +0100

    C'è la funzione che anonimizza il file di log

--- main.py
+++ main.py
@@ -24,4 +24,27 @@ def tab_utenti(input_data: dict, dst: str):
     out_file.close()
 
 
+def anonimizza(tab_utenti: str, dst: str, input_data: dict):
+    """
+    RIceve un .json con la lista di nomi indicizzati e un .json di log e anonimizza i log salvando il risultato in dst
+    :param tab_utenti: tabella con nomi utenti indicizzati
+    :param dst: destinazione di salvataggi dei log anonimizzati
+    :param input_data: file di log da anonimizzare
+    :return:
+    """
+    tab_indici = json.load(open(tab_utenti))
+
+    for log in range(len(input_data)):
+        for item in range(len(tab_indici)):
+            if input_data[log][1] == list(tab_indici.keys())[item]:
+                input_data[log][1] = tab_indici[input_data[log][1]]
+
+    out_file = open(dst, "w")
+
+    json.dump(data, out_file, indent=3)
+
+    out_file.close()
+
+
 tab_utenti(data, "tabella_utenti.json")
+anonimizza("tabella_utenti.json", "log_anonimizzato.json", data)
