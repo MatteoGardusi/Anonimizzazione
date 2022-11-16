1cbd561db1708c453324e52032fab76c398fbf76
Matteo Gardusi <matteo.gardusigmail.com>
2022-11-16 18:19:56 +0100

    Update comments

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ def scrivi_file_JSON(fname, data_to_be_written, indent=3):
     fout.close()
 
 
-# import da riga di comando
 parser = ArgumentParser(
-    description="Programma che anonimizza una lista di log e salva la corrispondenza tra nomi e codici assegnati")
-parser.add_argument('file_input',
-                    help='Path del file da anonimizzare',
+    description="Programma che anonimizza una lista di log fornita e archivia le corrispondenze tra nome e "
+                "identificatore")
+parser.add_argument('input_file',
+                    help='File contenente la lista di log da anonimizzare',
                     type=str)
-parser.add_argument('-t', '--tab_output',
-                    help='Path del file in cui salvare la tabella; se non indicato, il default è '
+parser.add_argument('-t', '--output_tab',
+                    help='Path di destinazione del file in cui salvare la tabella di corrispondenze. Default:'
                          './results/tabella_utenti.json',
                     type=str,
                     default='./results/tabella_utenti.json')
-parser.add_argument('-o', '--file_output',
-                    help='Path del file in cui salvare la lista anonimizzata; se non indicato, il default è '
+parser.add_argument('-o', '--output_file',
+                    help='Path di destinazione del file contentente la lista di log anonimizzata. Default: '
                          './results/log_anonimizzato.json',
                     type=str,
                     default='./results/log_anonimizzato.json')
@@ -37,7 +37,7 @@ parser.add_argument('-o', '--file_output',
 args = parser.parse_args()
 
 # leggere il file di log (lista di liste di stringhe)
-data = leggi_file_JSON(args.file_input)
+data = leggi_file_JSON(args.input_file)
 
 
 def tab_utenti(input_data: dict, dst: str):
@@ -45,7 +45,7 @@ def tab_utenti(input_data: dict, dst: str):
     Riceve il dict dei log e resituisce una tabella che associa un nome ad un indice
     :param input_data: data log fornito
     :param dst: path del file.json di destinazione
-    :return:
+    :return: None
     """
     index_tab = {}
 
@@ -63,7 +63,7 @@ def anonimizza(tab_utenti: str, dst: str, input_data: dict):
     :param tab_utenti: tabella con nomi utenti indicizzati
     :param dst: destinazione di salvataggi dei log anonimizzati
     :param input_data: file di log da anonimizzare
-    :return:
+    :return: None
     """
     tab_indici = load(open(tab_utenti))
 
@@ -79,7 +79,7 @@ def elimina_utente_coinvolto(src: str):
     """
     riceve un file di log e elimina il campo "Utente coinvolto"
     :param src: path file di log
-    :return:
+    :return: None
     """
     input_data = load(open(src))
     for log in range(len(input_data)):
@@ -88,8 +88,8 @@ def elimina_utente_coinvolto(src: str):
     scrivi_file_JSON(src, input_data)
 
 
-tab_utenti(data, args.tab_output)
+tab_utenti(data, args.outpu_tab)
 
-anonimizza(args.tab_output, args.file_output, data)
+anonimizza(args.output_tab, args.output_file, data)
 
-elimina_utente_coinvolto(args.file_output)
+elimina_utente_coinvolto(args.output_file)
